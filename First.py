962044c48f1c86f7a57bcbdd4762ef0dca69f71c
sandeep misra <sandeepmisrainca@gmail.com>
2022-01-10 14:02:34 -0800

    data frame

--- First.py
+++ First.py
@@ -7,5 +7,6 @@ data = {
   'py-score': [88.0, 79.0, 81.0, 80.0, 68.0, 61.0, 84.0] }
 
 row_labels = [101, 102, 103, 104, 105, 106, 107]
+# how to convert a data frame
 df = pd.DataFrame(data=data, index=row_labels)
 print(df)
